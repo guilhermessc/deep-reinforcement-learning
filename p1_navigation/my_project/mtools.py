8c1de31ccc4625239d514a8c344042350531f90e
Guilherme Sales Santa Cruz <guilhermessantacruz@gmail.com>
2020-04-23 14:43:41 +0200

    feat: Add Q-Neural-Network
    This NN learns the Q function for a given RL problem.
    Note: hyperparameters must be tuned.

--- p1_navigation/my_project/mtools.py
+++ p1_navigation/my_project/mtools.py
@@ -51,3 +51,47 @@ class ReplayBuffer:
 		return (states, actions, rewards, next_states, dones)
 
 
+class QNN:
+
+	def __init__(self, input_size, output_size, hidden_layers=[15, 8], activation=['relu', 'relu', 'linear']):
+
+		# Create the neural network model
+		model = Sequential()
+		layers = np.concatenate([hidden_layers, [output_size]])
+
+		# Create input layer
+		model.add(Dense(hidden_layers[0], input_dim=input_size, activation=activation[0]))
+
+		# Stack the layers after the first
+		for layer, activ in zip(layers[1:], activation[1:]):
+			model.add(Dropout(0.1)) # Add dropout in between layers
+			model.add(Dense(layer, activation=activ))
+
+		model.compile(loss='mean_absolute_error', optimizer='rmsprop', metrics=['accuracy'])
+
+		self.model = model
+
+	def get(self, x):
+		''' Return the Q-Value for each action from a given state x '''
+		
+		# Transform x to Keras input format
+		if len(np.shape(x)) == 1:
+			x = np.array([x])
+	
+		y = self.model.predict(x)
+
+		if len(y) == 1:
+			return y[0] # return one item for a simple prediction 
+		return y # return a batch for batch prediction
+
+	def update(self, x, loss):
+		
+		# Compute desired Y-update value
+		y_pred = self.get(x)
+		y_real += np.array(loss)
+
+		# Train on the batch many times to have a significant impact on the gradient
+		# TODO: refactor this
+		for i in range(100):
+			self.model.train_on_batch(x, y_real)
+
